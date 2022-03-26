1fcad78ddcaa1c6c886e08fe67bafd9382910edc
Baimempatlima <97828149+baimempatlima@users.noreply.github.com>
2022-03-26 13:42:17 +0700

    Update controller.js

--- app/product_v3/controller.js
+++ app/product_v3/controller.js
@@ -22,64 +22,93 @@ const index = (req, res) => {
 };
 
 const view = (req, res) => {
-  const { id } = req.params;
-  db.collection("products")
-    .findOne({ _id: ObjectId(id) })
-    .then((result) => res.send(result))
-    .catch((error) => res.send(error));
+  const id = req.params.id;
+  db.collection("products").findOne({ _id: ObjectId(id) }, (error, product) => {
+    if (error) {
+      res.send({ message: "not found", error });
+    } else {
+      res.send(product);
+    }
+  });
 };
 
 const store = (req, res) => {
-  const { name, price, stock, status } = req.body;
+  const createProduct = req.body;
   const image = req.file;
   if (image) {
     const target = path.join(__dirname, "../../uploads", image.originalname);
     fs.renameSync(image.path, target);
-    // res.sendFile(target);
-    // const { id } = req.params;
-    //  `http://localhost:3000/public/${image.originalname}`
-    db.collection("products")
-      .insertOne({ name, price, stock, status, image_url: `${req.protocol}://${req.headers.host}/public/${encodeURI(image.originalname)}` })
-      .then((result) => res.send(result))
-      .catch((error) => res.send(error));
+    createProduct.image = {
+      image_url: `${req.protocol}://${req.headers.host}/public/${encodeURI(image.originalname)}`,
+    };
+  } else {
+    createProduct.image = {
+      image_url: null,
+    };
   }
+
+  db.collection("products").insertOne(createProduct, (error, product) => {
+    if (error) {
+      res.status(404).send({
+        message: "failed",
+        error,
+      });
+    } else {
+      res.status(200).send({
+        message: "new data added successfully",
+        product,
+      });
+    }
+  });
 };
+
 
 const updateData = (req, res) => {
-  const { name, price, stock, status } = req.body;
+  const updateData = req.body;
   const image = req.file;
-  const { id } = req.params;
+
   if (image) {
     const target = path.join(__dirname, "../../uploads", image.originalname);
     fs.renameSync(image.path, target);
 
-    db.collection("products")
-      .findOneAndUpdate(
-        {
-          _id: ObjectId(id),
-        },
-        {
-          $set: {
-            name,
-            price,
-            stock,
-            status,
-            image_url: image ? `${req.protocol}://${req.headers.host}/public/${encodeURI(image.originalname)}` : null,
-          },
-        }
-      )
-      .then((result) => res.send(result))
-      .catch((err) => res.send(err));
+    updateData.image = {
+      image_url: `${req.protocol}://${req.headers.host}/public/${encodeURI(image.originalname)}`,
+    };
   }
+
+  db.collection("products").findOneAndUpdate({ _id: ObjectId(req.params.id) }, { $set: updateData }, { new: true }, (error, product) => {
+    if (error) {
+      res.status(404).send({
+        message: "failed",
+        error,
+      });
+    } else {
+      res.status(200).send({
+        message: "update data successfully updated",
+        product,
+      });
+    }
+  });
 };
+
 
 const destroy = (req, res) => {
   const id = req.params.id;
-  db.collection("products")
-    .deleteOne({ _id: ObjectId(id) })
-    .then((result) => res.send(result))
-    .catch((error) => res.send(error));
+  db.collection("products").deleteOne({ _id: ObjectId(id) }, (error, product) => {
+    if (error) {
+      res.status(404).send({
+        message: "failed",
+        error,
+      });
+    } else {
+      res.status(200).send({
+        message: "data deleted successfully",
+        product,
+      });
+    }
+  });
 };
+
 
 module.exports = {
   index,
